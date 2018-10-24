a01a5a561b86071b6089295cccd29eab9afdea5f
narith-minami <narith.for.work@gmail.com>
2018-10-24 12:11:04 +0900

    #4 fix twitter oauth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,45 +9,43 @@ class LoginController extends Controller
 {
     public function twitter() {
        $twitterAuth = new TwitterOAuth(env("TWITTER_CONSUMER_KEY"), env("TWITTER_CONSUMER_SECRET"));
-       $request_token = $twitterAuth->getRequestToken(env("TWITTER_OAUTH_CALLBACK"));
+       $request_token = $twitterAuth->oauth('oauth/request_token', array('o_auth'=>env("TWITTER_OAUTH_CALLBACK")));
 
        session(array(
          'requestToken'=>$request_token['oauth_token'],
          'requestTokenSecret'=>$request_token['oauth_token_secret']
        ));
 
-       if(isset($_GET['authorizeBoolean']) && $_GET['authorizeBoolean'] != '')
-       $bAuthorizeBoolean = false;
-       else
-       $bAuthorizeBoolean = true;
-
-       //Authorize url を取得
-       $sUrl = $twitterAuth->getAuthorizeURL($sToken, $bAuthorizeBoolean);
+       $url = $twitter->url('oauth/authenticate', array(
+                   'oauth_token' => $request_token['oauth_token']
+               ));
 
        return redirect($sUrl);
     }
 
     public function twitterCallback() {
-      $request_token = [];
-      $request_token['oauth_token'] = session['requestToken'];
-      $request_token['oauth_token_secret'] = session['requestTokenSecret'];
+      $oauth_token = session['requestToken'];
+      $oauth_token_secret = session['requestTokenSecret'];
 
-      if(isset($_GET['oauth_verifier']) && $_GET['oauth_verifier'] != ''){
-      	$sVerifier = $_GET['oauth_verifier'];
-      }else{
-      	echo 'oauth_verifier error!';
-      	exit;
+      # request_tokenが不正な値だった場合エラー
+      if ($request->has('oauth_token') && $oauth_token !== $request->oauth_token) {
+          return Redirect::to('/login');
       }
+      $twitter = new TwitterOAuth($oauth_token,$oauth_token_secret);
 
-      //OAuth トークンも用いて TwitterOAuth をインスタンス化
-      $connection = new TwitterOAuth(env("TWITTER_CONSUMER_KEY"), env("TWITTER_CONSUMER_SECRET"), $request_token['oauth_token'], $request_token['oauth_token_secret']);
+      $token = $twitter->oauth('oauth/access_token', array(
+          'oauth_verifier' => $request->oauth_verifier,
+          'oauth_token' => $request->oauth_token,
+      ));
 
-      //oauth_verifierを使ってAccess tokenを取得
-      $oAccessToken = $connection->getAccessToken($sVerifier);
+      # access_tokenを用いればユーザー情報へアクセスできるため、それを用いてTwitterOAuthをinstance化
+      $twitter_user = new TwitterOAuth(
+          config('TWITTER_CONSUMER_KEY'),
+          config('TWITTER_CONSUMER_SECRET'),
+          $token['oauth_token'],
+          $token['oauth_token_secret']
+      );
 
-      //取得した値をSESSIONに格納
-      session['oauth_token'] = 			$oAccessToken['oauth_token'];
-      session['oauth_token_secret'] = 	$oAccessToken['oauth_token_secret'];
       return redirect('/list');
     }
 }
