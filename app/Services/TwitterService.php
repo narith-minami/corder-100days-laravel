211b6b4459e4dc3e0d37011cb2a8b29c327a913d
narith-minami <narith.for.work@gmail.com>
2018-10-24 16:45:17 +0900

    #26 debug tweets

--- app/Services/TwitterService.php
+++ app/Services/TwitterService.php
@@ -23,6 +23,7 @@ class TwitterService
       // $access_token_secret);
 
     $oObj = $twitter->get("search/tweets", ["q" => $search_word,"lang" => $lang,"result_type"=>"resent","count"=>"100"]);
+    var_dump($oObj);
     return $this->convertViewData($oObj);
   }
 
@@ -60,7 +61,7 @@ class TwitterService
        'tweetLink'=>$tweetLink,'tweetURL'=>$tweetURL,'isFavorited'=>$isFavorited,'heartIcon'=>$heartIcon,'favClass'=>$favClass
        ];
      }
-
+     var_dump($tweets);
      return $tweets;
  }
 
